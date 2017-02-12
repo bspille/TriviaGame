e091dc374bdf504fd47d4d832ec5dc6cca2c27c5
Benjamin Spille <Benjamin Spille>
2017-02-12 10:45:56 -0500

    Completed file system and some initial scripts and text.

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+var trivia = {
+
+	display: $("#game-zone"),
+
+
+	// set variables here followed by game funtions
+
+	// try to use parameters more and keep the codes simple
+
+	// game must have a sarting function
+
+	// game must show one question and answers set at a time
+
+	// game must have timed intervals to advance the game or player answers to advance the game
+
+	// game must display status of answers between question congrad u correct, times up, u wrong the answer is
+
+	// game must display report after the game has ended, number of correct answers, number of wrong answers, option to restart
+
+	// game needs a bank of question and answer objects with one correct answer and three wrong answers
+
+	// the game needs to loop through the array of objects random if possible with random ordering of question answers
+
+	// note the events that need to be timed out and time intervals
+
+
+
+}
+$(document).ready(function() {
+//set initial function here
+});
