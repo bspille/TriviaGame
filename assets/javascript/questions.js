24575f7822e1d04385d260a40afc1c4439a181aa
Benjamin Spille <Benjamin Spille>
2017-02-12 16:08:27 -0500

    Added a array of questions in a seperate js page

---
+++ assets/javascript/questions.js
@@ -0,0 +1,73 @@
+var questions = [
+// each object holds a question property and answer property
+// the correct answer will alway be place first at index 0 of the answer array
+	{	question_1: "What is the most important part of programming?",
+		answers: ["Taking the first step.",
+				"Knowing how to code.",
+				"Being a certified programmer.",
+				"Learning everything there is to know about programming."]
+	},
+
+	{	question_2: "What is the primary use of regular expressions?",
+		answers: ["Validation by searching for patterns.",
+				"Expressions used on a regular bases.",
+				"Checks for regular conditionsin the function of the code.",
+				"regulates expressions."]
+},
+
+	{	question_3: "What does a switch do?",
+		answers: ["Takes in a single variable and checks it against multiple conditions for different outputs.",
+				"Turns on the lights.",
+				"Switch from one function to the next.",
+				"Turn off your computer."]
+	},
+
+	{	question_4: "What is a good use of Objects when programming?",
+		answers: ["Act as a container for all variables and fucntions.",
+				"To provide visual inspiration.",
+				"To provide support.",
+				"Paper weights."]
+	},
+
+	{	question_5: "What is a local variable?",
+		answers: ["A vaiable that is part of a function definition.",
+				"A variable that changes with the local community.",
+				"A variable named local.",
+				"A variable that can be accessed on you local computer."]
+	},
+
+	{	question_6: "What is a global variable?",
+		answers: ["A variable that is can be access by any function that scope extends to the window object.",
+				"A variable that can be accessed around the globe.",
+				"A variable that changes depending on the regoin of the world where you live.",
+				"A variable named global."]
+	},
+
+	{	question_7: "What is the main purpose of if statements?",
+		answers: ["Provide conditional checks within there scope.",
+				"Wrap entire functions.",
+				"Allow else statements to be used",
+				'A wish like, "If I had a million dollars".']
+		},
+
+	{	question_8: "What is a good use of for loops?",
+		answers: ["Cloneing array values.",
+				"Running programs in endless cycles.",
+				"practicing Your golf swing.",
+				"generating console.log's."]
+	},
+
+	{	question_9: "What is one way to increase the scope of a function?",
+		answers: ["Create a reference to the object you wish to grant access inside the scope.",
+				"Extend a pipe.",
+				"Use class functions to increase the scope.",
+				'make use of key word "self".']
+	},
+
+	{	question_10: "What is scope?",
+		answers: ["The referencing range of a function.",
+				 "A mouth wash.", 
+				 "An object used to see long distance's.",
+				 'The key word "this".'],
+	}
+] // end of the questions array
