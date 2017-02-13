4b9d52a91b56ec830dae70e3636ffc077779e519
Benjamin Spille <Benjamin Spille>
2017-02-12 21:26:11 -0500

    Added some random generator with some function definition

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,12 @@ var trivia = {
 	indexI: [],
 	// array of index numbers for the answers array in each questions object
 	indexJ: [],
+	// question load point
+	question: [],
+	// answer load point
+	answer: [],
+	// value given to the answer
+	value: [],
 	// counts the rounds
 	round: 0,
 	// ends the cylcles
@@ -30,23 +36,49 @@ var trivia = {
 						trivia.wrong = [];
 						trivia.indexI = [];
 						trivia.indexJ = [];
+						trivia.question = [];
 						trivia.round = 0;
 						trivia.end = false;
 						// sets initial values to index arrays
 						for (var i = 0; i < questions.length; i++) {
 							trivia.indexI.push(i);
-							for ( var j = 0; j < questions[i].answers.length; j++) {
-								trivia.indexJ.push(j);
-							};
 						};
-						$('.start').click(trivia.rounds);
+						$('.start').click(trivia.rounds); 
 					},
 
+	randomI: 	function() {
+					// random selection of questions from questions array with no repeats
+					var al = indexI.length--;
+					var ri = Math.round(Math.ramdom() * al);
+					trivia.question = indexI[ri];
+					trivia.indexI.splice(ri, 1);
+					// set value for indexJ array
+					for (var i = 0; i < questions[trivia.question].answers.length; i++) {
+						trivia.indexJ.push(i);
+					};
+				},
+
+	randomJ:  	function() {
+					// randomly selects answers with no repeats
+					var al = indexJ.length--;
+					var rj = Math.round(Math.random() * al);
+					trivia.answer  = trivia.indexJ[rj]
+					trivia.indexJ.splice(rj, 1);
+				},
+
 	rounds: 	function() {
-					trivia.round++;
+				
 					trivia.display.empty();
 					if (!end) {
 						setTimeout(trivia.after, 15000);
+						trivia.randomI().
+						trivia.display.append('<h2 class="question-display">' + questions[trivia.ri].question + '</h2>');
+						for (var i = 0; i < questions[trivia.question].answers.length; i++) {
+							trivia.randomJ();
+							trivia.display.append('<p class="answers" value="' + trivia.value + '">' +
+													questions[trivia.question].answers[trivia.answer] +
+													'</p>');
+						}
 
 					};
 					else {
@@ -55,12 +87,14 @@ var trivia = {
 				},
 
 	after: 		function() {
+
 				trivia.display.empty();
 					if (!end) {
 						setTimeout(trivia.rounds, 5000);
 						
 					};
-					if (trivia.round >= questions.length) {
+					trivia.round++;
+					if (trivia.round == questions.length) {
 						trivia.end = true;
 					};
 				},
@@ -70,22 +104,15 @@ var trivia = {
 				}
 
 
-
+	// started random generators for questions and answers still need a value generator and a flag for correct answers
 
 	// try to use parameters more and keep the codes simple
-
-	// game must show one question and answers set at a time
 
 	// game must have timed intervals to advance the game or player answers to advance the game
 
 	// game must display status of answers between question congrad u correct, times up, u wrong the answer is
 
 	// game must display report after the game has ended, number of correct answers, number of wrong answers, option to restart
-
-
-	// the game needs to loop through the array of objects random if possible with random ordering of question answers
-
-	// note the events that need to be timed out and time intervals
 
 
 
