cde09c4f5f516dc399dac75ac6c1458de71934f0
Benjamin Spille <Benjamin Spille>
2017-02-12 19:10:50 -0500

    Added base game functions definitions incomplete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,78 @@
 var trivia = {
+	// questions array hold objects with a question string property and a answers array property 
+	// correct answers are at index 0
 
+	// div where the game will show on the screen
 	display: $("#game-zone"),
+	// logs correct answers
+	correct: [],
+	// logs wrong answers
+	wrong: [],
+	// array of index numbers for the questions array objects
+	indexI: [],
+	// array of index numbers for the answers array in each questions object
+	indexJ: [],
+	// counts the rounds
+	round: 0,
+	// ends the cylcles
+	end: false,
+
+	initial: 		function() {
+						// displays initial text and start button
+						trivia.display.append("<h1 class='rmText'>Test Your coding knowedge</h1>");
+						trivia.display.append("<button class='start'>Begin</button>");	
+						trivia.start();			
+					},
+
+	start: 			function() {
+						// clears empty values
+						trivia.correct = [];
+						trivia.wrong = [];
+						trivia.indexI = [];
+						trivia.indexJ = [];
+						trivia.round = 0;
+						trivia.end = false;
+						// sets initial values to index arrays
+						for (var i = 0; i < questions.length; i++) {
+							trivia.indexI.push(i);
+							for ( var j = 0; j < questions[i].answers.length; j++) {
+								trivia.indexJ.push(j);
+							};
+						};
+						$('.start').click(trivia.rounds);
+					},
+
+	rounds: 	function() {
+					trivia.round++;
+					trivia.display.empty();
+					if (!end) {
+						setTimeout(trivia.after, 15000);
+
+					};
+					else {
+						// end results
+					}
+				},
+
+	after: 		function() {
+				trivia.display.empty();
+					if (!end) {
+						setTimeout(trivia.rounds, 5000);
+						
+					};
+					if (trivia.round >= questions.length) {
+						trivia.end = true;
+					};
+				},
+				
+	results: 	function() {
+				trivia.display.empty()
+				}
 
 
-	// set variables here followed by game funtions
+
 
 	// try to use parameters more and keep the codes simple
-
-	// game must have a sarting function
 
 	// game must show one question and answers set at a time
 
@@ -17,7 +82,6 @@ var trivia = {
 
 	// game must display report after the game has ended, number of correct answers, number of wrong answers, option to restart
 
-	// game needs a bank of question and answer objects with one correct answer and three wrong answers
 
 	// the game needs to loop through the array of objects random if possible with random ordering of question answers
 
@@ -26,6 +90,5 @@ var trivia = {
 
 
 }
-$(document).ready(function() {
-//set initial function here
-});
+// when the doc is ready run the start function
+$(document).ready(trivia.initial);
